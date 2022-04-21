d07581f1ca81e63cec2d29cfb84fd882b9943b79
Ilya Ploshchik <ilyaploshchik@gmail.com>
2022-04-21 22:51:32 +0200

    removed jupytertext reference

--- 2dv50e.py
+++ 2dv50e.py
@@ -1,17 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
 
 import pandas as pd
 import umap
