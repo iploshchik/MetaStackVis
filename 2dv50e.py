6e5d57f35934e02b5e304f0d3fb91ec42aa0e5f0
Ilya Ploshchik <ilyaploshchik@gmail.com>
2022-04-24 00:03:00 +0200

    updated UMAP chart

--- 2dv50e.py
+++ 2dv50e.py
@@ -39,8 +39,19 @@ def create_UMAP_chart(df_probabilities, algo_nr):
     umap_embedding = umap_model.fit_transform(df_probabilities)
     #convert umap_embedding to dataframe
     df_umap = pd.DataFrame(umap_embedding, columns=['UMAP_1', 'UMAP_2'])
-    # Add model name
+    # Add model name and ID
     df_umap['algorithm_nr'] = algo_nr
+    df_umap['model_id'] = df_model['model_id']
+    # Add model specific metrics
+    df_umap['accuracy'] = df_model['mean_test_accuracy']
+    df_umap['precision'] = df_model['mean_test_precision_weighted']
+    df_umap['recall'] = df_model['mean_test_recall_weighted']
+    df_umap['roc_auc_score'] = df_model['mean_test_roc_auc_ovo_weighted']
+    df_umap['geometric_mean_score'] = df_model['geometric_mean_score_weighted']
+    df_umap['matthews_corrcoef'] = df_model['matthews_corrcoef']
+    df_umap['f1_weighted'] = df_model['f1_weighted']
+    df_umap['log_loss'] = df_model['log_loss']
+
     #match algo_nr with algos keys
     df_umap['algorithm_name'] = df_umap['algorithm_nr'].map(algos)
     # add overall performance data to umap
@@ -48,7 +59,12 @@ def create_UMAP_chart(df_probabilities, algo_nr):
     # re-scale df.performance in scale from 0 to 1 and save as new column for better visualization
     df_umap['performance_scaled'] = preprocessing.MinMaxScaler().fit_transform(df_umap['performance'].values.reshape(-1,1))
     # create new column for text of points
-    df_umap['text'] = df_umap['algorithm_name'] + '<br>' + 'Performance: ' + df_umap['performance'].astype(str)
+    df_umap['text'] = df_umap['algorithm_name'] + '<br>' + 'Performance: ' + \
+            df_umap['performance'].astype(str) + '%' + '<br>' + 'Model ID: ' + df_umap['model_id'].astype(str) + '<br>' + 'Accuracy: ' + \
+            df_umap['accuracy'].astype(str) + '%' + '<br>' + 'Precision: ' + df_umap['precision'].astype(str) + '%' + '<br>' + 'Recall: ' + \
+            df_umap['recall'].astype(str) + '%' + '<br>' + 'ROC AUC: ' + df_umap['roc_auc_score'].astype(str) + '<br>' + 'Geometric Mean: ' + \
+            df_umap['geometric_mean_score'].astype(str) + '<br>' + 'Matthews Correlation: ' + df_umap['matthews_corrcoef'].astype(str) + '<br>' + 'F1: ' + \
+            df_umap['f1_weighted'].astype(str) + '<br>' + 'Log Loss: ' + df_umap['log_loss'].astype(str)
     # create new column for color coding of points
     df_umap['color'] = df_umap['performance_scaled'].apply(lambda x: 'rgb(' + str(int(x*255)) + ','+ 
             str(int(x*255)) + ','+ str(int(x*255)) + ')')
@@ -56,13 +72,20 @@ def create_UMAP_chart(df_probabilities, algo_nr):
     # Plot UMAP
     symbols = ['circle', 'square', 'x', 'cross', 'diamond', 'star', 'hexagram', 'triangle-right', 'triangle-left', 'triangle-down', 'triangle-up']
 
-    fig = px.scatter(df_umap, x='UMAP_1', y='UMAP_2', color='performance_scaled', hover_name='text',
-            symbol = df_umap['algorithm_name'], symbol_sequence = symbols, labels={'performance_scaled':'Performance'},
+    fig = px.scatter(df_umap, x='UMAP_1', y='UMAP_2', color='performance', hover_name='text',
+            symbol = df_umap['algorithm_name'], symbol_sequence = symbols, labels=dict(UMAP_1='', UMAP_2='', performance='Performance'),
             color_continuous_scale=px.colors.sequential.Viridis)
     fig.update_layout(title_text='UMAP Plot')
     fig.update_layout(showlegend=False)
     # Set marker symbol shape based on algorithm
-    fig.update_traces(marker=dict(size=10, opacity=0.9, line=dict(width=2, color='DarkSlateGrey')), selector=dict(mode='markers'))
+    fig.update_traces(marker=dict(size=10, opacity=0.9, line=dict(width=1, color='Black')), selector=dict(mode='markers'))
+    # Remove axis labels
+    fig.update_layout(xaxis=dict(showticklabels=False), yaxis=dict(showticklabels=False))
+    # add tooltip
+    fig.update_layout(hovermode='closest')
+    # define size of figure
+    fig.update_layout(width=600, height=550)
+
     st.plotly_chart(fig)
 
 
@@ -84,8 +107,3 @@ else:
         df_model = pd.read_csv(path + r'\topModels.csv')
         algo_nr = df_model.algorithm_id
         create_UMAP_chart(df_probabilities, algo_nr)
-
-
-
-
-
